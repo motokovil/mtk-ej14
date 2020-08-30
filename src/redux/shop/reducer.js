ddf3f3adfc01e2a2c42b28be89cffe080a80f24d
motokovil <65578314+motokovil@users.noreply.github.com>
2020-08-30 18:27:19 -0500

    ok

--- src/redux/shop/reducer.js
+++ src/redux/shop/reducer.js
@@ -96,7 +96,7 @@ const INITIAL_STATE = {
         return { ...previousState, cart: newCart };
       case "DELETE_PROD":
         const productsCart = [...previousState.cart];
-        if (productsCart[action.payload-1].quantity > 1) {
+        if (productsCart[action.payload-1].quantity >= 1) {
           let product = productsCart.findIndex(p=>p.id===action.payload)
           productsCart[product].quantity-=1;
         } else {
